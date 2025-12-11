1581cf72cd01b971cc7b9a3eb3991bec5b724ab3
Ben Wozniak <bw@sports-reference.com>
2025-12-11 16:22:15 -0600

    display auth status, signout

---
+++ app/pages/sign-out.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+const authStore = useAuthStore();
+
+onMounted(async () => {
+  await authStore.signOut();
+});
+</script>
+
+<template>
+  <div class="card bg-base-300 container mx-auto mt-4 flex min-h-72 flex-col items-center justify-center gap-4 text-center">
+    <span class="loading loading-spinner loading-xl" />
+  </div>
+</template>
