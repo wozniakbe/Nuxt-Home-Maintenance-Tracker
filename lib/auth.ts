0d7e7ac737909682425a2b59190cd3cdc644b0bc
Ben Wozniak <benrwozniak@gmail.com>
2025-12-12 07:51:47 -0600

    fix logged out get-session flow (#5)

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,24 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { createAuthMiddleware } from "better-auth/plugins";
 
 import db from "./db/index";
 import env from "./env";
 
 export const auth = betterAuth({
+  hooks: {
+    after: createAuthMiddleware(async (ctx) => {
+      if (ctx.path === "/get-session") {
+        if (!ctx.context.session) {
+          return ctx.json({
+            session: null,
+            user: null,
+          });
+        }
+        return ctx.json(ctx.context.session);
+      }
+    }),
+  },
   database: drizzleAdapter(db, {
     provider: "sqlite",
   }),
