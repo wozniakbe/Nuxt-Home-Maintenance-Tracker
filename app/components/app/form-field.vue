2a8de042e7a0ffa3e116a9e40b5388d70dcf3ba8
Ben Wozniak <benrwozniak@gmail.com>
2025-12-24 14:37:58 -0600

    do a million things so that we can add new items to the DB

---
+++ app/components/app/form-field.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  label: string;
+  name: string;
+  error?: string;
+  type?: "text" | "textarea" | "number";
+  disabled?: boolean;
+}>();
+</script>
+
+<template>
+  <fieldset class="fieldset">
+    <legend class="fieldset-legend">
+      {{ props.label }}
+    </legend>
+    <Field
+      :as="props.type === 'number' ? 'input' : props.type || 'input'"
+      :name="props.name"
+      :type="props.type || 'text'"
+      :disabled="disabled"
+      class="w-full"
+      :class="{
+        'input-error': props.error,
+        'input': !type || type === 'text' || type === 'number',
+        'textarea': type === 'textarea',
+      }"
+    />
+    <p v-if="props.error" class="fieldset-label text-error">
+      {{ props.error }}
+    </p>
+  </fieldset>
+</template>
+
+<style>
+
+</style>
