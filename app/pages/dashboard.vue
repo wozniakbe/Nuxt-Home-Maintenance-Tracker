5ac46840bcb4fa7b44cd7f81f51b981a9e939ea8
Ben Wozniak <benrwozniak@gmail.com>
2025-12-29 08:44:24 -0600

    listing items in sidebar, a bunch of state refactoring (#15)

--- app/pages/dashboard.vue
+++ app/pages/dashboard.vue
@@ -1,8 +1,14 @@
 <script lang="ts" setup>
 const isSidebarOpen = ref(true);
+const route = useRoute();
+const sidebarStore = useSidebarStore();
+const houseComponentsStore = useHouseComponentsStore();
 
 onMounted(() => {
   isSidebarOpen.value = localStorage.getItem("isSidebarOpen") === "true";
+  if (route.path !== "/dashboard") {
+    houseComponentsStore.refresh();
+  }
 });
 
 function toggleSidebar() {
@@ -43,6 +49,20 @@ function toggleSidebar() {
           icon="tabler:circle-plus-filled"
           href="/dashboard/add"
         />
+        <div v-if="sidebarStore.loading || sidebarStore.sidebarItems.length" class="divider" />
+        <div v-if="sidebarStore.loading" class="px-4">
+          <div class="skeleton h-4 w-full" />
+        </div>
+        <div v-if="!sidebarStore.loading && sidebarStore.sidebarItems.length" class="flex flex-col">
+          <SidebarButton
+            v-for="item in sidebarStore.sidebarItems"
+            :key="item.id"
+            :show-label="isSidebarOpen"
+            :label="item.label"
+            :icon="item.icon"
+            :href="item.href"
+          />
+        </div>
         <div class="divider" />
         <SidebarButton
           :show-label="isSidebarOpen"
