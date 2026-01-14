c1ad39ea9ec7aae68432430e733aac3696b87666
Ben Wozniak <benrwozniak@gmail.com>
2026-01-14 15:19:07 -0600

    show maintenance logs in sidebar, still figuring out a weird bug but workaround for now (#23)

--- app/stores/house-components.ts
+++ app/stores/house-components.ts
@@ -31,18 +31,33 @@ export const useHouseComponentsStore = defineStore("useHouseComponentsStore", ()
 
   effect(() => {
     if (houseComponents.value && HOUSE_COMPONENT_PAGES.has(route.name?.toString() || "")) {
-      sidebarStore.loading = false;
-      sidebarStore.sidebarItems = houseComponents.value.map(comp => ({
-        id: `component-${comp.id}`,
-        label: comp.name,
-        icon: "tabler:binoculars-filled",
-        to: { name: "dashboard-house-component-slug", params: { slug: comp.slug } },
-      }));
+      const sidebarItems: SidebarItem[] = [];
+
+      houseComponents.value.forEach((comp) => {
+        sidebarItems.push({
+          id: `component-${comp.id}`,
+          label: comp.name,
+          icon: "tabler:binoculars-filled",
+          to: { name: "dashboard-house-component-slug", params: { slug: comp.slug } },
+        });
+      });
+      sidebarStore.sidebarItems = sidebarItems;
     }
     else if (currentHouseComponent.value && CURRENT_HOUSE_COMPONENT_PAGES.has(route.name?.toString() || "")) {
-      sidebarStore.sidebarItems = [];
+      const sidebarItems: SidebarItem[] = [];
+
+      currentHouseComponent.value.maintenanceLogs.forEach((log) => {
+        sidebarItems.push({
+          id: `maintenance-log-${log.id}`,
+          label: log.name,
+          icon: "tabler:binoculars-filled",
+          // TODO: had to add the slug here but the tutorial didn't, I wonder if this is related to my bug endpoint problem
+          to: { name: "dashboard-house-component-slug-id", params: { slug: currentHouseComponent.value?.slug, id: log.id } },
+        });
+      });
+      sidebarStore.sidebarItems = sidebarItems;
     }
-    sidebarStore.loading = houseComponentsStatus.value === "pending";
+    sidebarStore.loading = houseComponentsStatus.value === "pending" || currentHouseComponentStatus.value === "pending";
   });
   return {
     houseComponents,
